10adf46ecb6013b855d81ff016feba40b6a8138c
manavsoni2701-01 <manavsoni2701@gmail.com>
2025-08-26 13:24:27 +0400

    Add files via upload

---
+++ Tasky1.py
@@ -0,0 +1,27 @@
+#task 1
+#basic mathematical operations
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+
+print("addition:", addition)
+print("subtraction:", subtraction)
+print("multiplication:", multiplication)
+print("division:", division)
+
+#task 2
+#Create a personalized greetings.
+
+first_name=input("Enter the first name: ")
+last_name=input('enter the last name: ')
+
+full_name=first_name+" "+last_name
+
+print("hello",full_name,"! Welcome to python program.")
+
+
